ccb8565776f836f264454e525f7aa222d3148e31
activenoisecontrol <vegas7546@gmail.com>
2020-09-17 09:24:11 +0000

    Done 5.2

--- exercises/05_basic_scripts/task_5_2.py
+++ exercises/05_basic_scripts/task_5_2.py
@@ -24,3 +24,30 @@ Out[1]: '11111111111111111111111111110000'
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input('Enter network address: ').split('/')
+address = ip[0].split('.')
+first = int(address[0])
+second = int(address[1])
+third = int(address[2])
+fourth = int(address[3])
+net_temp = '''
+    Network:
+    {0:<8}  {1:<8}  {2:<8}  {3:<8}
+    {0:08b}  {1:08b}  {2:08b}  {3:08b}
+    '''
+print(net_temp.format(first, second, third, fourth))
+
+mask = '1' * int(ip[1])
+mask = '{:<032}'.format(mask)
+moktet1 = int(mask[0:8],2)
+moktet2 = int(mask[8:16],2)
+moktet3 = int(mask[16:24],2)
+moktet4 = int(mask[24:],2)
+mask_temp = '''
+    Mask:
+    /{4:<}
+    {0:<8}  {1:<8}  {2:<8}  {3:<8}
+    {0:08b}  {1:08b}  {2:08b}  {3:08b}
+    '''
+print(mask_temp.format(moktet1, moktet2, moktet3, moktet4, ip[1]))
+
