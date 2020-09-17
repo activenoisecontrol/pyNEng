f5152d22edd0bc682794344d24ee8eac3a46912d
activenoisecontrol <vegas7546@gmail.com>
2020-09-17 14:18:33 +0000

    DONE 5.2

--- exercises/05_basic_scripts/task_5_2a.py
+++ exercises/05_basic_scripts/task_5_2a.py
@@ -39,3 +39,42 @@ bin_ip = "00001010000000010000000111000011"
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+ip = input('Enter network address: ').split('/')
+address = ip[0].split('.')
+
+mask = '1' * int(ip[1])
+mask = '{:<032}'.format(mask)
+moktet1 = int(mask[0:8],2)
+moktet2 = int(mask[8:16],2)
+moktet3 = int(mask[16:24],2)
+moktet4 = int(mask[24:],2)
+mask_temp = '''
+    Prefix: {4}
+    Mask:
+    {0:<8}  {1:<8}  {2:<8}  {3:<8}
+    {0:08b}  {1:08b}  {2:08b}  {3:08b}
+    '''
+
+first = int(address[0])
+second = int(address[1])
+third = int(address[2])
+fourth = int(address[3])
+
+ip_bin = '{:08b}{:08b}{:08b}{:08b}'.format(first, second, third, fourth)
+net_add = '{:>032}'.format(bin(int(ip_bin,2) & int(mask,2)).replace('0b',''))
+
+oktet1 = int(net_add[0:8],2)
+oktet2 = int(net_add[8:16],2)
+oktet3 = int(net_add[16:24],2)
+oktet4 = int(net_add[24:],2)
+net_temp = '''
+    IP address: {4}
+    Network:
+    {0:<8}  {1:<8}  {2:<8}  {3:<8}
+    {0:08b}  {1:08b}  {2:08b}  {3:08b}
+'''
+
+print(net_temp.format(oktet1, oktet2, oktet3, oktet4, ip[0]))
+
+print(mask_temp.format(moktet1, moktet2, moktet3, moktet4, ip[1]))
+
